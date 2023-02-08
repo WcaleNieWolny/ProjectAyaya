ef84ebbe55c0d10a0c86e02347d3253784b49300
WcaleNieWolny <isupermichael007@gmail.com>
2023-02-08 14:39:44 +0100

    Cargo fmt

--- ayaya_native/main/src/player/game_player.rs
+++ ayaya_native/main/src/player/game_player.rs
@@ -35,8 +35,8 @@ impl VideoCanvas {
         Self {
             vec: image.data.to_vec(),
             width: image.width as usize,
-            height: image.height as usize
-        } 
+            height: image.height as usize,
+        }
     }
 
     #[allow(dead_code)]
